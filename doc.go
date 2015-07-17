ee5227cdb529786863b67abf57258e0ced9b5b22
Timo Savola <timo@ninchat.com>
2015-07-17 11:48:40 +0300

    typo fix

--- doc.go
+++ doc.go
@@ -1,8 +1,8 @@
 // Package zmq4chan extends https://github.com/pebbe/zmq4 with channel I/O.
 //
-// Since ZeroMQ sockets are not thread-safe, they they cannot be used directly
-// for sending and receiving in different goroutines (e.g. when using a socket
-// type with unrestricted send/receive pattern).  This package interleaves the
+// Since ZeroMQ sockets are not thread-safe, they cannot be used directly for
+// sending and receiving in different goroutines (e.g. when using a socket type
+// with unrestricted send/receive pattern).  This package interleaves the
 // ZeroMQ calls safely, while providing a simple API.  The implementation uses
 // epoll, which makes it Linux-specific.
 //
